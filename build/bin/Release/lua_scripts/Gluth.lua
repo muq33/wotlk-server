30b3ff355e10a1cef4b7fcb9ab52c40dd5aa232f
Jean <83733853+Jean-Delfino@users.noreply.github.com>
2024-02-26 23:14:20 -0300

    Update Gluth behavior.

--- build/bin/Release/lua_scripts/Gluth.lua
+++ build/bin/Release/lua_scripts/Gluth.lua
@@ -1,5 +1,6 @@
 local Gluth = {id = 15932}
-local PoisonGas = {}
+local PoisonGas = {id = 37690}
+local ZombieChowCreature = {id = 16360}
 
 --Gluth (cachorro otário) boss_gluth.cpp
 --20% da vida do boss
@@ -18,17 +19,17 @@ local PoisonGas = {}
 --1s ele come bichos zombiezinhos perto dele
 
 --Sumona as posion cloud / poças baseado em porcentagem de vida, deixa de ser randomico.
---VETOR DE TRUE (has been spawned)
-
+local PoisonGas_Times = 
+{
+    ZOMBIE_CHOW_CREATURE = 15000
+}
 
 local Gluth_Spells =
 {
     MORTAL_WOUNDS               = 54378,
     MORTAL_WOUNDS_SPECIAL       = 20000, -- Pestilence, go for another group, plague
     ENRAGE                      = 54427,
-    MOVING_GAS_CREATURE         = 12000,
     DECIMATE                    = 28375,
-    ZOMBIE_CHOW_CREATURE        = 16360,
     --SPELL_RAGNAROS_SUBMERGE_EFFECT          = 21859,
 };
 
@@ -36,27 +37,85 @@ local Gluth_Spells_Times =
 {
     MORTAL_WOUNDS               = 10000,
     ENRAGE                      = 22000,
-    MOVING_GAS_SPAWN            = 12000,
     DECIMATE                    = 500,
 }
 
---DEFINE "BLOCKS" FOR POISON SPAWN
 local poisonGasPosition = {
-    {x = 3308.393066, y = -3184.520996, z = 350.969086},
-    {x = 3267.750977, y = -3171.457520, z = 350.969482},
-    {x = 3256.010742, y = -3130.549072, z = 350.969482},
-    {x = 3308.294678, y = -3131.699463, z = 350.969086}
+    {x = 3308.393066, y = -3184.520996, z = 350.969086, spawned = false, creature_ref = nil},
+    {x = 3267.750977, y = -3171.457520, z = 350.969482, spawned = false, creature_ref = nil},
+    {x = 3256.010742, y = -3130.549072, z = 350.969482, spawned = false, creature_ref = nil},
+    {x = 3308.294678, y = -3131.699463, z = 350.969086, spawned = false, creature_ref = nil}
 }
---
-local availableBlockPositions;
 
-function Gluth.MovingPoisonGas(eventId, delay, calls, creature)
-    --Summon at a random non occupied in the map 
-    --creature:SpawnCreature(Gluth_Spells.MOVING_GAS_CREATURE, )
+function PoisonGas.SpawnZombie(eventId, delay, calls, creature)
+    local x, y, z, o = creature:GetLocation()
+    creature:SpawnCreature(ZombieChowCreature.id,         
+    x,
+    y,
+    z, 0, 5);
 end
 
-function PoisonGas.SpawnZombie()
+--PoisonGas
+function PoisonGas.OnSpawn(event, creature)
+    creature:RegisterEvent(PoisonGas.SpawnZombie, PoisonGas_Times.ZOMBIE_CHOW_CREATURE, 0)
+end
+
+function PoisonGas.OnRemove(event, creature)
+    creature:RemoveEvents()
+end
+
+function PoisonGas.OnDied(event, creature, killer)
+    creature:RemoveEvents()
+end
+
+--ZombieChowCreature
+function ZombieChowCreature.OnRemove(event, creature)
+    creature:RemoveEvents()
+end
+
+function ZombieChowCreature.OnDied(event, creature, killer)
+    creature:RemoveEvents()
+end
+
+--Gluth
+function Gluth.SpawnPoisonGas(eventId, delay, calls, creature)
+    --Summon at a random non occupied in the map 
+    local poisonGas = FindNotSpawnedPosition(poisonGasPosition)
+
+    if  poisonGas == nil then
+        return
+    end
     
+    creature:SpawnCreature(PoisonGas.id,         
+        poisonGas.x,
+        poisonGas.y,
+        poisonGas.z, 0, 5);
+end
+
+function FindNotSpawnedPosition(positions)
+    local notSpawnedPositions = {} -- Table to store not spawned positions
+
+    -- Iterate through positions
+    for i, pos in ipairs(positions) do
+        if not pos.spawned then
+            table.insert(notSpawnedPositions, i) -- Store the index of not spawned position
+        end
+    end
+
+    -- If there are not spawned positions, return a random one
+    if #notSpawnedPositions > 0 then
+        local randomIndex = notSpawnedPositions[math.random(1, #notSpawnedPositions)]
+        return positions[randomIndex], randomIndex
+    else
+        return nil -- No not spawned positions found
+    end
+end
+
+function CleanAll()
+    for i, pos in ipairs(poisonGasPosition) do
+        pos.spawned = false
+        pos.creature_ref:Kill(pos.creature_ref)
+    end
 end
 
 function Gluth.MortalWounds(eventId, delay, calls, creature)
@@ -71,7 +130,7 @@ end
 
 
 function Gluth.OnEnterCombat(event, creature, target)
-    creature:RegisterEvent(Gluth.MovingPoisonGas, Gluth_Spells_Times.MOVING_GAS_SPAWN, 0)
+    creature:RegisterEvent(Gluth.SpawnPoisonGas, Gluth_Spells_Times.MOVING_GAS_SPAWN, 0)
     creature:RegisterEvent(Gluth.MortalWounds, Gluth_Spells_Times.MORTAL_WOUNDS, 0)
     creature:RegisterEvent(Gluth.EnrageEmpower, Gluth_Spells_Times.ENRAGE, 0)
     creature:RegisterEvent(Gluth.CheckDecimate, Gluth_Spells_Times.DECIMATE, 0)
@@ -94,13 +153,22 @@ function Gluth.CheckDecimate(eventId, delay, calls, creature )
 end
 
 function Gluth.OnLeaveCombat(event, creature)
+    CleanAll()
     creature:RemoveEvents()
 end
 
 function Gluth.OnDied(event, creature, killer)
+    CleanAll()
     creature:RemoveEvents()
 end
 
 RegisterCreatureEvent(Gluth.id, 1, Gluth.OnEnterCombat)
 RegisterCreatureEvent(Gluth.id, 2, Gluth.OnLeaveCombat)
 RegisterCreatureEvent(Gluth.id, 4, Gluth.OnDied)
+
+RegisterCreatureEvent(PoisonGas.id, 5, PoisonGas.OnSpawn)
+RegisterCreatureEvent(PoisonGas.id, 37, PoisonGas.OnRemove)
+RegisterCreatureEvent(PoisonGas.id, 4, PoisonGas.OnDied)
+
+RegisterCreatureEvent(ZombieChowCreature.id, 37, ZombieChowCreature.OnRemove)
+RegisterCreatureEvent(ZombieChowCreature.id, 4, ZombieChowCreature.OnDied)
